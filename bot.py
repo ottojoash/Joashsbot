67b0627f1f55aac01faa96848b9616081ee4dbca
ottojoash <louisjoshbricks@gmail.com>
2022-12-27 14:19:49 -0500

    code

---
+++ bot.py
@@ -0,0 +1,23 @@
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Set up the bot and get the update queue
+updater = Updater("YOUR_BOT_TOKEN", use_context=True)
+dispatcher = updater.dispatcher
+
+# Define the start command handler
+def start(update, context):
+  context.bot.send_message(chat_id=update.effective_chat.id, text="Hello, I am a bot. How can I help you today?")
+
+start_handler = CommandHandler("start", start)
+dispatcher.add_handler(start_handler)
+
+# Define the echo command handler
+def echo(update, context):
+  context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+echo_handler = MessageHandler(Filters.text, echo)
+dispatcher.add_handler(echo_handler)
+
+# Start the bot
+updater.start_polling()
