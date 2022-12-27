26127f6ac856ee2a05b586a075cce5d8a796c341
ottojoash <louisjoshbricks@gmail.com>
2022-12-27 14:31:55 -0500

    tele

--- telegrambot.py
+++ telegrambot.py
@@ -2,12 +2,13 @@ import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 # Set up the bot and get the update queue
+#place your bot token in the updater
 updater = Updater("5819005387:AAFlBtVCLXX4NHI8bMKPsyUhp97wNW4vgB0", use_context=True)
 dispatcher = updater.dispatcher
 
 # Define the start command handler
 def start(update, context):
-  context.bot.send_message(chat_id=update.effective_chat.id, text="Hello, I am a bot. How can I help you today?")
+  context.bot.send_message(chat_id=update.effective_chat.id, text="Hello, I am a Joashbot. How can I help you today?")
 
 start_handler = CommandHandler("start", start)
 dispatcher.add_handler(start_handler)
